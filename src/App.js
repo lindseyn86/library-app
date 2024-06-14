888f9917a45be422c0245c35fbf15f3feb70e53c
Lindsey Eom <linand02@protiviti.com>
2024-06-14 10:30:58 -0400

    put in CRUD functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
+
+
 
 function App() {
+  const API_URL = 'https://666c475449dbc5d7145d6d4b.mockapi.io/plants'
+
+  const [plants, setPlants] = useState([{}])
+
+  const [newPlantName, setNewPlantName] = useState('')
+  const [newSpeciesName, setNewSpeciesName] = useState('')
+  const [newLocation, setNewLocation] = useState('')
+  const [newSoilType, setNewSoilType] = useState('')
+
+  const [updatedPlantName, setUpdatedPlantName] = useState('')
+  const [updatedSpeciesName, setUpdatedSpeciesName] = useState('')
+  const [updatedLocation, setUpdatedLocation] = useState('')
+  const [updatedSoilType, setUpdatedSoilType] = useState('')
+
+  function getPlants(){
+    fetch(API_URL)
+    .then(data => data.json())
+    .then(data => setPlants(data))
+  }
+  useEffect(() => {
+    getPlants()  
+  }, [])
+
+  function deletePlant(id){
+    fetch(`${API_URL}/${id}`, {
+      method: 'DELETE'
+    })
+    .then(() => getPlants())
+  }
+  
+  function updatePlant(plantObject){
+
+    let updatedPlantObject = {
+      ...plantObject,
+      name: updatedPlantName,
+      speciesName: updatedSpeciesName,
+      location: updatedLocation,
+      soilType: updatedSoilType,
+    }
+
+    fetch(`${API_URL}/${plantObject.id}`, {
+      method: 'PUT',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(updatedPlantObject)
+    })
+    .then(() => getPlants())
+  }
+  
+  function postNewPlant(){
+
+    fetch(API_URL, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({
+        name: newPlantName,
+        speciesName: newSpeciesName,
+        location: newLocation,
+        soilType: newSoilType,
+      })
+    })
+    .then(() => getPlants())
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
